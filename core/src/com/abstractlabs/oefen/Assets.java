8ae3be27ed7ae0bc7e9bd20bbc4fc956fc10818d
Talha <dragoknight85@gmail.com>
2016-06-15 16:53:47 -0400

    Skeleton Animations

--- core/src/com/abstractlabs/oefen/Assets.java
+++ core/src/com/abstractlabs/oefen/Assets.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Assets {
@@ -11,14 +12,28 @@ public class Assets {
 	public static TextureRegion backgroundIngameRegion;
 
 	public static Texture tiles;
+	public static Texture skeleton;
 	public static TextureRegion voidTile;
 	public static TextureRegion grassTile;
 	public static TextureRegion dirtTile;
 	public static TextureRegion dirtCornerTile;
+	public static TextureRegion currentFrame;
+	public static TextureRegion[] walkFrames;
 	public static Animation waterTile;
 	public static Animation waterCornerTile;
 	public static Animation bridgeTile;
+	public static Animation skeleWalkUp;
+	public static Animation skeleWalkDown;
+	public static Animation skeleWalkRight;
+	public static Animation skeleWalkLeft;
+	public static Animation skeleAttackUp;
+	public static Animation skeleAttackLeft;
+	public static Animation skeleAttackDown;
+	public static Animation skeleAttackRight;
 	public static TextureRegion hoverTile;
+	
+	private static final int FRAME_COLS = 6;
+	private static final int FRAME_ROWS = 4;
 
 	public static Texture towers;
 	public static TextureRegion greenTower;
@@ -63,6 +78,75 @@ public class Assets {
 				new TextureRegion(tiles, 2*16, 6*16, 16, 16), 
 				new TextureRegion(tiles, 3*16, 6*16, 16, 16));
 		hoverTile = new TextureRegion(tiles, 0*16, 15*16, 16, 16);
+		
+		skeleton = loadTexture("gfx/skeleton_sprites.png");
+		skeleWalkUp = new Animation(0.5f,
+				new TextureRegion(skeleton, 0*64, 8*64, 64, 64),
+				new TextureRegion(skeleton, 1*64, 8*64, 64, 64),
+				new TextureRegion(skeleton, 2*64, 8*64, 64, 64),
+				new TextureRegion(skeleton, 3*64, 8*64, 64, 64),
+				new TextureRegion(skeleton, 4*64, 8*64, 64, 64),
+				new TextureRegion(skeleton, 5*64, 8*64, 64, 64),
+				new TextureRegion(skeleton, 6*64, 8*64, 64, 64),
+				new TextureRegion(skeleton, 7*64, 8*64, 64, 64),
+				new TextureRegion(skeleton, 8*64, 8*64, 64, 64));
+		skeleWalkLeft = new Animation(0.5f,
+				new TextureRegion(skeleton, 0*64, 9*64, 64, 64),
+				new TextureRegion(skeleton, 1*64, 9*64, 64, 64),
+				new TextureRegion(skeleton, 2*64, 9*64, 64, 64),
+				new TextureRegion(skeleton, 3*64, 9*64, 64, 64),
+				new TextureRegion(skeleton, 4*64, 9*64, 64, 64),
+				new TextureRegion(skeleton, 5*64, 9*64, 64, 64),
+				new TextureRegion(skeleton, 6*64, 9*64, 64, 64),
+				new TextureRegion(skeleton, 7*64, 9*64, 64, 64),
+				new TextureRegion(skeleton, 8*64, 9*64, 64, 64));
+		skeleWalkDown = new Animation(0.5f,
+				new TextureRegion(skeleton, 0*64, 10*64, 64, 64),
+				new TextureRegion(skeleton, 1*64, 10*64, 64, 64),
+				new TextureRegion(skeleton, 2*64, 10*64, 64, 64),
+				new TextureRegion(skeleton, 3*64, 10*64, 64, 64),
+				new TextureRegion(skeleton, 4*64, 10*64, 64, 64),
+				new TextureRegion(skeleton, 5*64, 10*64, 64, 64),
+				new TextureRegion(skeleton, 6*64, 10*64, 64, 64),
+				new TextureRegion(skeleton, 7*64, 10*64, 64, 64),
+				new TextureRegion(skeleton, 8*64, 10*64, 64, 64));
+		skeleWalkRight = new Animation(0.5f,
+				new TextureRegion(skeleton, 0*64, 11*64, 64, 64),
+				new TextureRegion(skeleton, 1*64, 11*64, 64, 64),
+				new TextureRegion(skeleton, 2*64, 11*64, 64, 64),
+				new TextureRegion(skeleton, 3*64, 11*64, 64, 64),
+				new TextureRegion(skeleton, 4*64, 11*64, 64, 64),
+				new TextureRegion(skeleton, 5*64, 11*64, 64, 64),
+				new TextureRegion(skeleton, 6*64, 11*64, 64, 64),
+				new TextureRegion(skeleton, 7*64, 11*64, 64, 64),
+				new TextureRegion(skeleton, 8*64, 11*64, 64, 64));
+		skeleAttackUp = new Animation(0.2f,
+				new TextureRegion(skeleton, 0*64, 4*64, 64, 64),
+				new TextureRegion(skeleton, 1*64, 4*64, 64, 64),
+				new TextureRegion(skeleton, 2*64, 4*64, 64, 64),
+				new TextureRegion(skeleton, 3*64, 4*64, 64, 64),
+				new TextureRegion(skeleton, 4*64, 4*64, 64, 64),
+				new TextureRegion(skeleton, 5*64, 4*64, 64, 64));
+		skeleAttackLeft = new Animation(0.2f,
+				new TextureRegion(skeleton, 0*64, 5*64, 64, 64),
+				new TextureRegion(skeleton, 1*64, 5*64, 64, 64),
+				new TextureRegion(skeleton, 2*64, 5*64, 64, 64),
+				new TextureRegion(skeleton, 3*64, 5*64, 64, 64),
+				new TextureRegion(skeleton, 4*64, 5*64, 64, 64));
+		skeleAttackDown = new Animation(0.2f,
+				new TextureRegion(skeleton, 0*64, 6*64, 64, 64),
+				new TextureRegion(skeleton, 1*64, 6*64, 64, 64),
+				new TextureRegion(skeleton, 2*64, 6*64, 64, 64),
+				new TextureRegion(skeleton, 3*64, 6*64, 64, 64),
+				new TextureRegion(skeleton, 4*64, 6*64, 64, 64),
+				new TextureRegion(skeleton, 5*64, 6*64, 64, 64));
+		skeleAttackRight = new Animation(0.2f,
+				new TextureRegion(skeleton, 0*64, 7*64, 64, 64),
+				new TextureRegion(skeleton, 1*64, 7*64, 64, 64),
+				new TextureRegion(skeleton, 2*64, 7*64, 64, 64),
+				new TextureRegion(skeleton, 3*64, 7*64, 64, 64),
+				new TextureRegion(skeleton, 4*64, 7*64, 64, 64),
+				new TextureRegion(skeleton, 5*64, 7*64, 64, 64));
 
 		//Towers
 		towers = loadTexture("gfx/towers.png");
@@ -78,6 +162,8 @@ public class Assets {
 
 		//Sounds
 		clickSound = Gdx.audio.newSound(Gdx.files.internal("sound/click.wav"));
+		
+		
 	}
 
 	public static void playSound (Sound sound) {
--- core/src/com/abstractlabs/oefen/Oefen.java
+++ core/src/com/abstractlabs/oefen/Oefen.java
@@ -2,13 +2,16 @@ package com.abstractlabs.oefen;
 
 import com.abstractlabs.oefen.screen.ScreenGame;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class Oefen extends Game {
 	public SpriteBatch batch;
 	
 	@Override
-	public void create () {
+	public void create () {	
 		batch = new SpriteBatch();
 		Settings.load();
 		Assets.load();
